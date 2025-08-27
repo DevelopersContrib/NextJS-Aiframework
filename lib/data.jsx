0f1a45d56204328f15f2533f84592b67656e1a0b
ronan <row_none@yahoo.com>
2025-08-26 19:41:02 -0700

    test

--- lib/data.jsx
+++ lib/data.jsx
@@ -33,12 +33,12 @@ export function ucfirst(str) {
 export async function getData() {
   const domain = getDomain();
   const url = process.env.CONTRIB_API1+`&domain=${domain}`
- const res = await fetch(url, {
-  mode: 'cors',
-  headers: {
-    'User-Agent': 'Mozilla/5.0'
-  }
-});
+const res = await fetch(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    },
+    next: { revalidate: 3600 }
+  });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
